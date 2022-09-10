6a8bd4e3eb25585f8c62de99034d36c02368fe53
Alberto Valiente Thoresen <46265925+albertovth@users.noreply.github.com>
2022-09-10 23:52:30 +0200

    Update PoderFutbolisticoInternacional.py

--- PoderFutbolisticoInternacional.py
+++ PoderFutbolisticoInternacional.py
@@ -145,7 +145,7 @@ def equipo_casa_input_():
     elif equipo_casa_input == "DC United":
         return "D.C. United is a professional soccer club based in Washington, D.C. that competes in the Eastern Conference of Major League Soccer"
     elif equipo_casa_input == "Atletico Madrid":
-        return "Club Atletico Madrid S.A.D."
+        return "atlético madrid professional soccer club"
     else:
         return equipo_casa_input
 
@@ -173,7 +173,7 @@ def equipo_visita_input_():
     elif equipo_visita_input == "DC United":
         return "D.C. United is a professional soccer club based in Washington, D.C. that competes in the Eastern Conference of Major League Soccer"
     elif equipo_visita_input == "Atletico Madrid":
-        return "Club Atletico Madrid S.A.D."
+        return "atlético madrid professional soccer club"
     else:
         return equipo_visita_input
 
