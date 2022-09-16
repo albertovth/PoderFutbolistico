09ddcbba093bd0ce1ee0944cc464ac1716ca6d3e
Alberto Valiente Thoresen <46265925+albertovth@users.noreply.github.com>
2022-09-16 14:47:18 +0200

    Update PoderFutbolisticoInternacional.py

--- PoderFutbolisticoInternacional.py
+++ PoderFutbolisticoInternacional.py
@@ -332,9 +332,9 @@ try:
     goles_esperados_equipo_casa = ((equipo_casa_of) + (equipo_visita_def)) / 2
     goles_esperados_equipo_visita = ((equipo_visita_of) + (equipo_casa_def)) / 2
 
-    goles_esperados_equipo_casa_redondeado = math.ceil(goles_esperados_equipo_casa * 100) / 100.0)
+    goles_esperados_equipo_casa_redondeado = (math.ceil(goles_esperados_equipo_casa * 100) / 100.0)
 
-    goles_esperados_equipo_visita_redondeado = math.ceil(goles_esperados_equipo_visita * 100) / 100.0)
+    goles_esperados_equipo_visita_redondeado = (math.ceil(goles_esperados_equipo_visita * 100) / 100.0)
 
 except IndexError:
     goles_esperados_equipo_casa_redondeado = 1
